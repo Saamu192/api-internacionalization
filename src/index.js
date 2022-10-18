1e4ab0fc1759f92d91c27c3e0e743383fbe70c19
Saamu192 <samuelpr192@gmail.com>
2022-10-18 01:02:58 -0300

    initialize terminal

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-import readline from "readline";
 import database from "./../database.json" assert { type: "json" };
 import Person from "./persons.js";
+import TerminalController from "./terminal.controller.js";
+
+const DEFAULT_LANG = "pt-BR";
+
+const terminalController = new TerminalController();
+terminalController.initializeTerminal(database, DEFAULT_LANG);
