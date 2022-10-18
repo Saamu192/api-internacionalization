8c12f1263a8bcf63d6d2517f33a87079246c3443
Saamu192 <samuelpr192@gmail.com>
2022-10-18 01:09:17 -0300

    feat: generate new data from string

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import Person from "./persons.js";
 import TerminalController from "./terminal.controller.js";
 
 const DEFAULT_LANG = "pt-BR";
+const STOP_TERM = ":q";
 
 const terminalController = new TerminalController();
 terminalController.initializeTerminal(database, DEFAULT_LANG);
